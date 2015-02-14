544b15c6ac947e3086202a3db074af6fb6748ccc
joshuabamboo <joshuaowens011@gmail.com>
2015-02-13 22:03:18 -0500

    again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  skip_before_filter  :verify_authenticity_token
   protect_from_forgery with: :exception
+  skip_before_filter  :verify_authenticity_token
 end
